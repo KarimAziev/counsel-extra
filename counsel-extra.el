7431bfcc68a58291e1b964c5ded6825d9c6f1d77
KarimAziev <karim.aziiev@gmail.com>
2024-01-16 15:03:08 +0200

    Release version 0.3.0

--- counsel-extra.el
+++ counsel-extra.el
@@ -5,7 +5,7 @@
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/counsel-extra
 ;; Keywords: lisp, convenience
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "28.1") (ivy "0.14.0") (counsel "0.14.0"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
